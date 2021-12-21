b851b38ed006abbbc520a47a10c6821387e354b3
shynehot05 <gr8sunshynehot@gmail.com>
2021-12-21 07:02:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var johnHeight= 187;
+var samHeight =175;
+var johnAge=44;
+var samAge=32;
+var scores1=samHeight+samAge+5;
+var scores2=johnHeight+johnAge+5;
+// get the game winner
+ 
+function gamewinner(scores1, scores2) {
+    if(scores1 > scores2){
+        console.log("sam is the winner");
+    }
+    else if(scores1==scores2){
+        console.log("sam and john draw the game");
+    }
+
+    else{ console.log("john is the winner")};
+    
+       
+       
+    }
+
+    
+
+ 
+
+/*var tom= 3;
+var gem=6;
+ var sol= 3*6;
+ console.log(tom ++ gem);*/
